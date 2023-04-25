96e7e75b44837bdee34ad865e6f8af4a864ce6f8
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-25 07:48:15 +0100

    Update un-global-platform.py

--- un-global-platform.py
+++ un-global-platform.py
@@ -22,7 +22,7 @@ documents = loader.load_data(ytlinks=['https://www.youtube.com/watch?v=kUPCGrTUD
 
 index = GPTSimpleVectorIndex.from_documents(documents)
 
-st.set_page_config(page_title="UN Global Platform & Open Source)
+st.set_page_config(page_title="UN Global Platform & Open Source")
 st.title("UN Global Platform")
 st.sidebar.markdown("# Query this video using AI")
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
